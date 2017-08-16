7fea3c20607bcc1940834a84a65cbbcf03ff52b0
meetbill <meetbill@163.com>
2017-08-16 16:14:55 +0800

    update:检查近期登陆系统情况，修复只输出密码登陆以及密钥登陆情况

--- sh_menu/Function/1_check_linux/login_ip.sh
+++ sh_menu/Function/1_check_linux/login_ip.sh
@@ -7,12 +7,12 @@
 #!/bin/bash
 CUR_DIR=$(cd `dirname $0`; pwd)
 cd ${CUR_DIR}
-cat /var/log/secure | awk '/Accepted password/{print $0 $(NF-3)}' |awk 'BEGIN{split("Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec",k," ");for(i=1;i<13;i++){m[k[i]]=i}}{$1=m[$1];print "/"$1"/"$2" "$3" "$(NF-5)" "$(NF-3)" ""Password"}' >/tmp/login.log
-cat /var/log/secure | awk '/Accepted publickey/{print $0 $(NF-3)}' |awk 'BEGIN{split("Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec",k," ");for(i=1;i<13;i++){m[k[i]]=i}}{$1=m[$1];print "/"$1"/"$2" "$3" "$(NF-5)" "$(NF-3)" ""Key"}'>>/tmp/login.log
+cat /var/log/secure | awk '/Accepted/{print $0 $(NF-3)}' |awk 'BEGIN{split("Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec",k," ");for(i=1;i<13;i++){m[k[i]]=i}}{$1=m[$1];print "/"$1"/"$2"\t"$3"\t"$7"\t"$(NF-5)"\t"$(NF-3)" "}' > /tmp/login.log
 cat /tmp/login.log | while read myline
-do 
+do
     echo -n $myline
-    IP=`echo $myline | awk  '{print $4}'`
-    SITE=`python ./query_ip.py ${IP}` 
-    echo "-----"$SITE
+    IP=`echo $myline | awk  '{print $5}'`
+    SITE=`python ./query_ip.py ${IP}`
+    echo "--------------"$SITE
 done
+
